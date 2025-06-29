4becc4372fc1fa183659148c991475821256620f
Delaksan-Sritharan <delaksan@gmail.com>
2025-06-29 22:00:28 +0530

    feat: update StudentsView component to include table structure

--- frontend/src/component/student/StudentsView.js
+++ frontend/src/component/student/StudentsView.js
@@ -3,7 +3,19 @@ import React from 'react'
 const StudentsView = () => {
     return (
         <section>
-
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+            </table>
         </section>
     )
 }
