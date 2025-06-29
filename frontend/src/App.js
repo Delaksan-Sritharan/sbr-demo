87562cc208fe4523a704c58c11e073fafe71da5a
Delaksan-Sritharan <delaksan@gmail.com>
2025-06-29 21:52:54 +0530

    added fronend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Home from './Home';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Welcome to our frontend</h2>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
