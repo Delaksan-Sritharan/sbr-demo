b82aa8f475ea234c7a5886925a3719b93c4a4dc2
Delaksan-Sritharan <delaksan@gmail.com>
2025-06-29 22:11:52 +0530

    feat: enhance StudentsView component with student data display and table structure

--- frontend/src/component/student/StudentsView.js
+++ frontend/src/component/student/StudentsView.js
@@ -1,20 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const StudentsView = () => {
+
+    const [students, setStudents] = useState([]);
     return (
         <section>
             <table>
                 <thead>
                     <tr>
-                        <th></th>
-                        <th></th>
-                        <th></th>
-                        <th></th>
-                        <th></th>
-                        <th></th>
-                        <th></th>
+                        <th>ID</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Department</th>
+                        <th>Action</th>
                     </tr>
                 </thead>
+                <tbody>
+                    {students.map((student, index) => (
+                        <tr key={student.id}>
+                            <th scope='row' key={index}>
+                                {index + 1}
+                            </th>
+                            <td>{student.firstName}</td>
+                            <td>{student.lastName}</td>
+                            <td>{student.email}</td>
+                            <td>{student.department}</td>
+                            <td>View</td>
+                            <td>Update</td>
+                            <td>Delete</td>
+                        </tr>
+                    ))}
+
+                </tbody>
             </table>
         </section>
     )
