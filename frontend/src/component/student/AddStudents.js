37643fd626116499c3b753670c6c4a31c33d93fc
Delaksan-Sritharan <delaksan@gmail.com>
2025-07-02 11:31:46 +0530

    Enhance AddStudents component with form structure and input for first name

--- frontend/src/component/student/AddStudents.js
+++ frontend/src/component/student/AddStudents.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 const AddStudents = () => {
     return (
-        <div className='col-sm-8 py-2'>
+        <div className='col-sm-8 py-2 px-5'>
+            <form>
+                <div className='input-group mb-5'>
+                    <label
+                        className='input-group-text'
+                        htmlFor='firstName'
+                    >First Name</label>
+                    <input className='form-control col-sm-6'
+                        type='text'
+                        name='firstName'
+                        id='firstName' >
+                    </input>
+
+                </div>
+            </form>
 
         </div>
     )
